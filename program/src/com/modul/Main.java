076fedbdf2b08742b12c3d69e7155f13a6e0154d
adii83 <siapa83anda@gmail.com>
2024-02-23 19:05:43 +0700

    Tugas Codelab Modul 1

--- program/src/com/modul/Main.java
+++ program/src/com/modul/Main.java
@@ -7,23 +7,27 @@ import java.time.Period;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner Input= new Scanner(System.in);
 
         System.out.print("Masukan Nama Anda : ");
-        String nama= Input.nextLine();
+        String nama = Input.nextLine();
 
-        System.out.print("Masukan Kelamin Anda (P/L) : ");
-        char kelamin= Input.next().charAt(0);
-        Input.nextLine();
-
-        if (!(kelamin == 'L' || kelamin == 'l' || kelamin == 'P' || kelamin == 'p')) {
-            System.out.println("Inputan Tidak Valid (Masukkan P/L)!!");
-            System.exit(0);
+        while (true) {
+            System.out.print("Masukan Kelamin Anda (P/L) : ");
+            char kelamin= Input.next().charAt(0);
+            Input.nextLine();
+            if (kelamin == 'L' || kelamin == 'l') {
+                System.out.println("Jenis Kelamin : Laki-Laki");
+                break;
+            } else if (kelamin == 'P' || kelamin == 'p'){
+                System.out.println("Jenis Kelamin : Perempuan");
+                break;
+            }else {
+                System.out.println("Inputan Tidak Valid (Masukkan P/L)!!");
+            }
         }
-
         System.out.print("Tanggal Lahir (yyyy-mm-dd) : ");
-        String inputLahir= Input.nextLine();
+        String inputLahir = Input.nextLine();
         LocalDate lahir= LocalDate.parse(inputLahir);
 
         LocalDate timeReal=LocalDate.now();
@@ -35,12 +39,6 @@ public class Main {
         System.out.print("\n");
         System.out.println("========================");
         System.out.println("Nama : "+nama);
-
-        if (kelamin=='L'||kelamin=='l'){
-            System.out.println("Jenis Kelamin : Laki-Laki");
-        }else{
-            System.out.println("Jenis Kelamin : Perempuan");
-        }
 
         System.out.println("Umur Anda : "+tahun+" tahun "+bulan+" bulan");
 
